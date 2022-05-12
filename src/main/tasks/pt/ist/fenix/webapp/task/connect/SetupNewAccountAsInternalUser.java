fbd281996ef1dc0735a71b7a8437f7874786c124
Luis Cruz <luis.cruz@tecnico.pt>
2022-05-12 15:07:15 +0100

    Script for setting up validated internal test accounts

--- src/main/tasks/pt/ist/fenix/webapp/task/connect/SetupNewAccountAsInternalUser.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/connect/SetupNewAccountAsInternalUser.java
@@ -13,7 +13,6 @@ import org.fenixedu.admissions.ist.domain.UserAccountInfo;
 import org.fenixedu.admissions.ist.util.IdentificationDocumentConverter;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.core.domain.User;
-import org.fenixedu.bennu.core.domain.UserProfile;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
 import org.fenixedu.connect.domain.Account;
 import org.fenixedu.connect.domain.Identity;
@@ -38,6 +37,7 @@ public class SetupNewAccountAsInternalUser extends CustomTask {
         }
         final Person person = personFor(account);
         person.ensureOpenUserAccount();
+        person.getUser().setIdentity(account.getIdentity());
         final Teacher teacher = person.getTeacher() == null
                 ? new Teacher(person) : person.getTeacher();
         TeacherAuthorization.createOrUpdate(teacher,
