f5c851c44852f2ef851823c97abb0c709fb984d1
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-06-23 16:18:44 +0100

    Run CustomTask HackFinancialDocumentsAndOperations

--- src/main/tasks/pt/ist/fenix/webapp/task/accounting/HackFinancialDocumentsAndOperations.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/accounting/HackFinancialDocumentsAndOperations.java
@@ -2,6 +2,7 @@ package pt.ist.fenix.webapp.task.accounting;
 
 import org.fenixedu.academic.domain.accounting.AccountingTransaction;
 import org.fenixedu.academic.domain.accounting.Event;
+import org.fenixedu.academic.domain.accounting.Refund;
 import org.fenixedu.bennu.scheduler.custom.WriteCustomTask;
 import pt.ist.fenixframework.FenixFramework;
 
@@ -12,14 +13,15 @@ public class HackFinancialDocumentsAndOperations extends WriteCustomTask {
 
     @Override
     public void runTask() throws Exception {
+        final Refund refund = FenixFramework.getDomainObject("571277895012736");
         final AccountingTransaction toDelete = FenixFramework.getDomainObject("564470371935314");
         final Event event = toDelete.getEvent();
         final Set<AccountingTransaction> others = event.getAccountingTransactionsSet().stream()
                 .filter(tx -> tx != toDelete)
                 .peek(tx -> tx.setEvent(null))
                 .collect(Collectors.toSet());
-        toDelete.getTransactionDetail().setPaymentMethod(null);
-        toDelete.delete();
+        refund.delete();
+//        toDelete.delete();
         others.forEach(tx -> tx.setEvent(event));
     }
 
