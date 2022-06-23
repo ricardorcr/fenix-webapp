70e34ec14bc2e3209e227c8ec00b400534f834f2
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-06-23 16:32:10 +0100

    Run CustomTask HackFinancialDocumentsAndOperations

--- src/main/tasks/pt/ist/fenix/webapp/task/accounting/HackFinancialDocumentsAndOperations.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/accounting/HackFinancialDocumentsAndOperations.java
@@ -1,18 +1,19 @@
 package pt.ist.fenix.webapp.task.accounting;
 
-import org.fenixedu.academic.domain.accounting.AccountingTransaction;
 import org.fenixedu.academic.domain.accounting.Event;
 import org.fenixedu.academic.domain.accounting.Refund;
+import org.fenixedu.academic.util.Money;
 import org.fenixedu.bennu.scheduler.custom.WriteCustomTask;
+import pt.ist.fenixedu.domain.SapRequest;
+import pt.ist.fenixedu.domain.SapRequestType;
+import pt.ist.fenixedu.giaf.invoices.SapEvent;
 import pt.ist.fenixframework.FenixFramework;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class HackFinancialDocumentsAndOperations extends WriteCustomTask {
 
     @Override
     public void runTask() throws Exception {
+/*
         final Refund refund = FenixFramework.getDomainObject("571277895012736");
         final AccountingTransaction toDelete = FenixFramework.getDomainObject("564470371935314");
         final Event event = toDelete.getEvent();
@@ -23,6 +24,13 @@ public class HackFinancialDocumentsAndOperations extends WriteCustomTask {
         refund.delete();
 //        toDelete.delete();
         others.forEach(tx -> tx.setEvent(event));
+ */
+
+        final Event event = FenixFramework.getDomainObject("571204880564868");
+        final Refund refund = FenixFramework.getDomainObject("571277895012808");
+        final SapEvent sapEvent = new SapEvent(event);
+        final SapRequest sapRequest = sapEvent.fakeSapRequest(SapRequestType.REIMBURSEMENT, "NR0", new Money("0.07"), null);
+        sapRequest.setRefund(refund);
     }
 
 }
