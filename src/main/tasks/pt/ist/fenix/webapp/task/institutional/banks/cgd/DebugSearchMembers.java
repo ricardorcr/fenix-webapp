72756f4a21d1539bb901ef971d5c3353c3f54949
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-06-24 15:35:34 +0100

    Run CustomTask DebugSearchMembers

---
+++ src/main/tasks/pt/ist/fenix/webapp/task/institutional/banks/cgd/DebugSearchMembers.java
@@ -0,0 +1,22 @@
+package pt.ist.fenix.webapp.task.institutional.banks.cgd;
+
+import com.qubit.solution.fenixedu.integration.cgd.webservices.CgdIntegrationService;
+import com.qubit.solution.fenixedu.integration.cgd.webservices.messages.member.SearchMemberInput;
+import com.qubit.solution.fenixedu.integration.cgd.webservices.messages.member.SearchMemberOutput;
+import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class DebugSearchMembers extends ReadCustomTask {
+    @Override
+    public void runTask() throws Exception {
+        final SearchMemberInput input = new SearchMemberInput();
+        input.setDocumentID("PT230124674");
+        input.setDocumentType(501);
+        final SearchMemberOutput output = new CgdIntegrationService().searchMember(input);
+        taskLog("%s = %s%n", output.getReplyCode(), output.getMemberInfo().stream()
+                .map(info -> info.getName())
+                .collect(Collectors.joining("; ")));
+    }
+}
