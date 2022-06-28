390cde5cced637306b0fbf568a79a2d399a56475
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-06-28 11:55:01 +0100

    Run CustomTask DebugBoardApplicationWrongCycle

---
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/DebugBoardApplicationWrongCycle.java
@@ -0,0 +1,49 @@
+package pt.ist.fenix.webapp.task.admissions;
+
+import com.google.gson.JsonObject;
+import org.fenixedu.academic.domain.degreeStructure.CycleType;
+import org.fenixedu.admissions.domain.AdmissionProcessTarget;
+import org.fenixedu.admissions.domain.Application;
+import org.fenixedu.bennu.core.util.TransactionalThread;
+import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+import pt.ist.fenixframework.FenixFramework;
+
+public class DebugBoardApplicationWrongCycle extends ReadCustomTask {
+
+    @Override
+    public void runTask() throws Exception {
+        try {
+            taskLog("Test 1... as is.");
+            TransactionalThread.runTx(false, () -> {
+                final Application application = FenixFramework.getDomainObject("571415333968181");
+                application.board();
+                throw new Error("Abort TX");
+            });
+        } catch (final Throwable t) {
+            taskLog("Fail: %s%n", t.getMessage());
+            if (t.getCause() != null) {
+                taskLog("   -> %s%n", t.getCause().getMessage());
+            }
+        }
+
+        try {
+            taskLog("Test 2... hack cycle type.");
+            TransactionalThread.runTx(false, () -> {
+                final Application application = FenixFramework.getDomainObject("571415333968181");
+                final AdmissionProcessTarget target = application.getAdmissionProcessTarget();
+                final JsonObject outcome = target.getOutcomeConfigJson();
+                outcome.addProperty("cycleType", CycleType.SECOND_CYCLE.name());
+                target.setOutcomeConfig(outcome.toString());
+                application.board();
+                throw new Error("Abort TX");
+            });
+        } catch (final Throwable t) {
+            taskLog("Fail: %s%n", t.getMessage());
+            if (t.getCause() != null) {
+                taskLog("   -> %s%n", t.getCause().getMessage());
+            }
+        }
+
+        throw new Error("Abort TX");
+    }
+}
