952b9b009e8e39dd4733a26002fac65855e31269
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-06-29 13:15:05 +0100

    Run CustomTask AttemptDocumentRegenerationForRequestedFixTins

--- src/main/tasks/pt/ist/fenix/webapp/task/accounting/AttemptDocumentRegenerationForRequestedFixTins.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/accounting/AttemptDocumentRegenerationForRequestedFixTins.java
@@ -30,7 +30,7 @@ public class AttemptDocumentRegenerationForRequestedFixTins extends WriteCustomT
                 .forEach(event -> taskLog("Multiple debts in event = %s%n", event.getExternalId()));
  */
 
-        Stream.of("569628627567553"
+        Stream.of("571557067913136", "571557067913153"
                 //"564805379294555", "571200585597521", "571200585597522", "564805379294556", "846280356005434"
         )
 //        Arrays.stream(string("spam_tin_pc.txt").split("\n"))
