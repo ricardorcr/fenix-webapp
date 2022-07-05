ba71430ab8138694069ab52625620e9dfcb783e9
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-07-05 12:46:42 +0100

    Run CustomTask DebugSearchMembers

--- src/main/tasks/pt/ist/fenix/webapp/task/institutional/banks/cgd/DebugSearchMembers.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/institutional/banks/cgd/DebugSearchMembers.java
@@ -21,6 +21,12 @@ public class DebugSearchMembers extends ReadCustomTask {
 
     @Override
     public void runTask() throws Exception {
+        System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dumpTreshold", "999999");
+
         final SearchMemberInput input = new SearchMemberInput();
         input.setDocumentID("230124674");
         input.setDocumentType(501);
