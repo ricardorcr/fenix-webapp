8de5b77d582243b26fe64b53f80d70eed59d5c53
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-07-05 17:15:32 +0100

    Run CustomTask DebugIdentities

---
+++ src/main/tasks/pt/ist/fenix/webapp/task/DebugIdentities.java
@@ -0,0 +1,18 @@
+package pt.ist.fenix.webapp.task;
+
+import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+import org.fenixedu.connect.domain.ConnectSystem;
+
+public class DebugIdentities extends ReadCustomTask {
+
+    @Override
+    public void runTask() throws Exception {
+        ConnectSystem.getInstance().getIdentitySet().stream()
+                .map(identity -> identity.getPersonalInformation())
+                .filter(personalInformation -> personalInformation.getIdentificationDocument() == null)
+                .forEach(personalInformation -> {
+                    taskLog("No ID Document for identity %s%n", personalInformation.getIdentity().getExternalId());
+                });
+    }
+
+}
