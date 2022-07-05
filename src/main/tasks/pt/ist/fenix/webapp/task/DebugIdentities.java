976f5f8850eab002fa8557076ec2ce2f61034005
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-07-05 17:20:49 +0100

    Run CustomTask DebugIdentities

--- src/main/tasks/pt/ist/fenix/webapp/task/DebugIdentities.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/DebugIdentities.java
@@ -8,10 +8,13 @@ public class DebugIdentities extends ReadCustomTask {
     @Override
     public void runTask() throws Exception {
         ConnectSystem.getInstance().getIdentitySet().stream()
-                .map(identity -> identity.getPersonalInformation())
-                .filter(personalInformation -> personalInformation.getIdentificationDocument() == null)
-                .forEach(personalInformation -> {
+                .filter(identity -> identity.getPersonalInformation() == null)
+//                .map(identity -> identity.getPersonalInformation())
+//                .filter(personalInformation -> personalInformation.getIdentificationDocument() == null)
+//                .forEach(personalInformation -> {
+                  .forEach(identity -> {
                     //taskLog("No ID Document for identity %s%n", personalInformation.getIdentity().getExternalId());
+                      taskLog("No personal information for: %s%n", identity.getExternalId());
                 });
     }
 
