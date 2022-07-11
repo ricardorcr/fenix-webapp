5c8c3a7d0150d9d2ff5ac3d57e3152b27c131b5a
Ricardo da Conceição Rodrigues <ricardo.rodrigues@tecnico.ulisboa.pt>
2022-07-11 17:04:46 +0100

    Run CustomTask CorrectSapRequest

--- src/main/tasks/pt/ist/fenix/webapp/CorrectSapRequest.java
+++ src/main/tasks/pt/ist/fenix/webapp/CorrectSapRequest.java
@@ -10,17 +10,17 @@ public class CorrectSapRequest extends CustomTask {
 
     @Override
     public void runTask() throws Exception {
-        SapRequest sapRequest = FenixFramework.getDomainObject("852658382927267");
+        SapRequest sapRequest = FenixFramework.getDomainObject("852658382927268");
         final String date = new DateTime().toString("yyyy-MM-dd HH:mm:ss");
         changeDate(sapRequest, date);
     }
 
     private void changeDate(final SapRequest sr, final String date) {
         final JsonObject request = sr.getRequestAsJson();
-        final JsonObject workingDocument = request.getAsJsonObject("workingDocument");
-        workingDocument.addProperty("documentDate", date);
-        workingDocument.addProperty("dueDate", date);
-        workingDocument.addProperty("entryDate", date);
+//        final JsonObject workingDocument = request.getAsJsonObject("workingDocument");
+//        workingDocument.addProperty("documentDate", date);
+//        workingDocument.addProperty("dueDate", date);
+//        workingDocument.addProperty("entryDate", date);
         final JsonObject paymentDocument = request.getAsJsonObject("paymentDocument");
         paymentDocument.addProperty("paymentDate", date);
         sr.setRequest(request.toString());
