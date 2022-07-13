8a20555f9e621a1d95ddb34de4297b0ba231b835
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-07-13 11:31:18 +0100

    Run CustomTask HackAccounts

--- src/main/tasks/pt/ist/fenix/webapp/task/connect/HackAccounts.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/connect/HackAccounts.java
@@ -1,5 +1,6 @@
 package pt.ist.fenix.webapp.task.connect;
 
+import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
 import org.fenixedu.connect.domain.Account;
 import org.fenixedu.connect.domain.Identity;
@@ -10,8 +11,8 @@ public class HackAccounts extends CustomTask {
 
     @Override
     public void runTask() throws Exception {
-        final Account account1 = FenixFramework.getDomainObject("853087879175300");
-        account1.setEmail("ist419033@ulisboa.pt");
+        final Account account1 = FenixFramework.getDomainObject("1697512810064735");
+        account1.getIdentity().setUser(User.findByUsername("ist419033"));
     }
 
 }
