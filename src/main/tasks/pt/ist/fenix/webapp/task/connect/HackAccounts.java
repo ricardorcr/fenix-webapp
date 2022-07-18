2c9b6025c2823a7b542a935ff68191e8785a8532
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-07-18 16:33:52 +0100

    Run CustomTask HackAccounts

--- src/main/tasks/pt/ist/fenix/webapp/task/connect/HackAccounts.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/connect/HackAccounts.java
@@ -1,41 +1,21 @@
 package pt.ist.fenix.webapp.task.connect;
 
-import org.fenixedu.academic.domain.Person;
-import org.fenixedu.academic.domain.student.Student;
-import org.fenixedu.bennu.core.domain.User;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
 import org.fenixedu.connect.domain.Account;
-import org.fenixedu.connect.domain.Identity;
-import org.fenixedu.connect.domain.identification.PersonalInformation;
+import org.fenixedu.connect.domain.identification.TaxInformation;
 import pt.ist.fenixframework.FenixFramework;
 
 public class HackAccounts extends CustomTask {
 
     @Override
     public void runTask() throws Exception {
-        final User user1 = User.findByUsername("ist30047");
-        final User user2 = User.findByUsername("ist419033");
-
-//        final Account account1 = FenixFramework.getDomainObject("1697512810064735");
-//        account1.getIdentity().setUser(User.findByUsername("ist419033"));
-
-        accept(user2, user1);
-    }
-
-    private void accept(User newUser, User oldUser) {
-        final Student studentFromNewUser = studentFor(newUser);
-        final Student studentFromOldUser = studentFor(oldUser);
-        if (studentFromNewUser != null && studentFromOldUser != null) {
-            studentFromOldUser.getRegistrationsSet().forEach(registration -> registration.setStudent(studentFromNewUser));
-            studentFromOldUser.getPerson().getEventsSet().stream().forEach(event -> event.setParty(studentFromNewUser.getPerson()));
-        } else if (studentFromOldUser != null) {
-            studentFromOldUser.setPerson(newUser.getPerson());
-        }
-    }
-
-    private Student studentFor(final User user) {
-        final Person person = user == null ? null : user.getPerson();
-        return person == null ? null : person.getStudent();
+        final Account account = FenixFramework.getDomainObject("1697512810074933");
+        final TaxInformation taxInformation = account.getIdentity().getPersonalInformation().getTaxInformation();
+        final JsonObject address = new JsonParser().parse(taxInformation.getAddressData()).getAsJsonObject();
+        address.addProperty("zipCode", "83324-050");
+        taxInformation.setAddressData(address.toString());
     }
 
 }
