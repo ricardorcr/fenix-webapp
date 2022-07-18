3367ed6843df256b3ed05a89e465fabe1fe4f8d8
Luis Cruz <luis.cruz@tecnico.pt>
2022-07-18 16:06:02 +0100

    Upgraded academic and ist. Block creation of events when user has inconsistant personal information. Custom fill client address for SAP invoices

--- src/main/java/pt/ist/fenix/webapp/servlet/FenixIstWebAppListener.java
+++ src/main/java/pt/ist/fenix/webapp/servlet/FenixIstWebAppListener.java
@@ -1,16 +1,30 @@
 package pt.ist.fenix.webapp.servlet;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.fenixedu.academic.domain.Person;
 import org.fenixedu.academic.domain.SchoolLevelType;
+import org.fenixedu.academic.domain.accounting.Event;
 import org.fenixedu.academic.domain.student.Student;
 import org.fenixedu.admissions.ist.util.QualificationLevelUtil;
 import org.fenixedu.bennu.core.domain.User;
+import org.fenixedu.bennu.core.json.JsonUtils;
 import org.fenixedu.bennu.core.util.CoreConfiguration;
 import org.fenixedu.bennu.core.util.TransactionalThread;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
+import org.fenixedu.connect.domain.Account;
+import org.fenixedu.connect.domain.ConnectSystem;
+import org.fenixedu.connect.domain.Identity;
+import org.fenixedu.connect.domain.identification.PersonalInformation;
+import org.fenixedu.connect.domain.identification.TaxInformation;
 import org.fenixedu.git.Repository;
 import org.fenixedu.ulisboa.integration.sas.service.process.AbstractFillScholarshipService;
 import pt.ist.fenix.webapp.Configuration;
+import pt.ist.fenixedu.giaf.invoices.SapEvent;
+import pt.ist.fenixedu.giaf.invoices.Utils;
 import pt.ist.fenixframework.FenixFramework;
+import pt.ist.standards.geographic.Planet;
+import pt.ist.standards.geographic.PostalCode;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -104,6 +118,55 @@ public class FenixIstWebAppListener implements ServletContextListener, Configura
                 });
             }
         }
+
+        Event.registerPersonalInformationCheck(person -> {
+            final PersonalInformation personalInformation = personalInformationFor(person);
+            if (personalInformation == null) {
+                throw new Error("Unable to get valid address information. No personal information found for person.");
+            }
+
+            final TaxInformation taxInformation = personalInformation.getTaxInformation();
+            if (taxInformation == null) {
+                throw new Error("Unable to find tax information for person.");
+            }
+
+            if (!taxInformation.isValid()) {
+                throw new Error("Tax information for person is not valid.");
+            }
+        });
+
+        SapEvent.ADDRESS_FILLER = (party, clientData) -> {
+            final String clientId = clientData.get("clientId").getAsString();
+            final String countryCode = clientId.substring(0, 2);
+
+            final PersonalInformation personalInformation = personalInformationFor((Person) party);
+            if (personalInformation == null) {
+                throw new Error("Unable to get valid address information. No personal information found for person.");
+            }
+
+            final TaxInformation taxInformation = personalInformation.getTaxInformation();
+            if (taxInformation == null) {
+                throw new Error("Unable to find tax information for person.");
+            }
+
+            if (!taxInformation.isValid()) {
+                throw new Error("Tax information for person is not valid.");
+            }
+
+            final JsonObject address = new JsonParser().parse(taxInformation.getAddressData()).getAsJsonObject();
+            final String line1 = JsonUtils.get(address, "firstLine");
+            final String line2 = JsonUtils.get(address, "secondLine");
+            final String street = line2 == null ? line1 : (line1 + ", " + line2);
+            final String location = JsonUtils.get(address, "location");
+            final String zipCode = JsonUtils.get(address, "zipCode");
+            final String district = districtFor(countryCode, zipCode);
+
+            clientData.addProperty("street", Utils.limitFormat(SapEvent.MAX_SIZE_ADDRESS, street));
+            clientData.addProperty("city", Utils.limitFormat(SapEvent.MAX_SIZE_CITY, location));
+            clientData.addProperty("region", district == null ? SapEvent.MORADA_DESCONHECIDO
+                    : Utils.limitFormat(SapEvent.MAX_SIZE_REGION, district));
+            clientData.addProperty("postalCode", zipCode);
+        };
     }
 
     @Override
@@ -138,6 +201,34 @@ public class FenixIstWebAppListener implements ServletContextListener, Configura
         } catch (final IOException e) {
             throw new Error(e);
         }
+    }
+
+    private PersonalInformation personalInformationFor(final Person person) {
+        final User user = person.getUser();
+        final Identity identity = user == null ? null : user.getIdentity();
+        Account account = identity == null ? null : ConnectSystem.getMostRelevantAccount(identity);
+        if (account == null && user != null) {
+            account = user.getAccount();
+        }
+        if (account == null) {
+            account = findByEvent(person);
+        }
+        return account == null ? null : ConnectSystem.getPersonalInformationFor(account);
+    }
+
+    private Account findByEvent(final Person person) {
+        return person.getEventsSet().stream()
+                .flatMap(event -> event.getApplicationSet().stream())
+                .filter(application -> application != null)
+                .map(application -> application.getAccount())
+                .findAny().orElse(null);
+    }
+
+    private String districtFor(final String countryCode, final String zipCode) {
+        final PostalCode postalCode = Planet.getEarth().getByAlfa2(countryCode).getPostalCode(zipCode);
+        final JsonObject details = postalCode.getDetails();
+        final String distrito = details == null ? null : JsonUtils.get(details, "Distrito");
+        return distrito;
     }
 
 }
