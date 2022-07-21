89a6506c8a073d87d7a1a03bbfb805edeb717d2d
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-07-21 19:06:12 +0100

    Run CustomTask DumpDegreeChangeSerializationInfo

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/DumpDegreeChangeSerializationInfo.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/DumpDegreeChangeSerializationInfo.java
@@ -1,5 +1,6 @@
 package pt.ist.fenix.webapp.task.admissions;
 
+import com.google.gson.JsonObject;
 import org.fenixedu.academic.domain.Grade;
 import org.fenixedu.academic.domain.Person;
 import org.fenixedu.academic.domain.student.Registration;
@@ -7,6 +8,7 @@ import org.fenixedu.academic.domain.student.RegistrationDataByExecutionYear;
 import org.fenixedu.academic.domain.student.Student;
 import org.fenixedu.academic.domain.student.curriculum.ICurriculum;
 import org.fenixedu.admissions.domain.AdmissionProcess;
+import org.fenixedu.admissions.util.DynamicForm;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
 import org.fenixedu.commons.spreadsheet.Spreadsheet;
@@ -57,16 +59,26 @@ public class DumpDegreeChangeSerializationInfo extends ReadCustomTask {
                                 .multiply(ects)
                                 .divide(divisor, 2, RoundingMode.HALF_EVEN);
                     }
+                    final JsonObject form = application.getAdmissionProcessTarget().getAdmissionProcess().getFormDataJson();
+                    final DynamicForm dynamicForm = new DynamicForm(form).withData(application.getDataObject().getAsJsonObject("formData"));
+                    final DynamicForm.Quantity highschool_average = dynamicForm.get("highschool_average");
+                    final DynamicForm.Quantity first_ingression_grade = dynamicForm.get("first_ingression_grade");
+                    final DynamicForm.Quantity second_ingression_grade = dynamicForm.get("second_ingression_grade");
 
                     final Spreadsheet.Row row = spreadsheet.addRow();
                     row.setCell("application", application.getExternalId());
                     row.setCell("isISTStudent", Boolean.toString(registration != null));
+                    row.setCell("username", registration == null ? "" : user.getUsername());
                     row.setCell("lastRegistration", registration == null ? "" : registration.getDegree().getSigla());
                     row.setCell("lastRegistrationState", registration == null ? "" : registration.getLastState().getStateType().getDescription());
                     row.setCell("enrolledYears", registration == null ? "" : Long.toString(enrolledYears));
                     row.setCell("weightedGrade", registration == null ? "" : finalGrade.getValue());
                     row.setCell("ects", registration == null ? "" : ects.toPlainString());
+                    row.setCell("highschool_average", highschool_average == null ? "" : highschool_average.value().toPlainString());
+                    row.setCell("first_ingression_grade", first_ingression_grade == null ? "" : first_ingression_grade.value().toPlainString());
+                    row.setCell("second_ingression_grade", second_ingression_grade == null ? "" : second_ingression_grade.value().toPlainString());
                     row.setCell("MA", registration == null ? "" : ma.toPlainString());
+
                 });
 
         final ByteArrayOutputStream stream = new ByteArrayOutputStream();
