bbb206ced5a3ea76ced3f1870427e8679d558ef4
Ricardo da Conceição Rodrigues <ricardo.rodrigues@tecnico.ulisboa.pt>
2022-07-25 17:53:12 +0100

    Run CustomTask FixWronglyCreatedRegistrationDatasMobility

--- src/main/tasks/pt/ist/fenix/webapp/FixWronglyCreatedRegistrationDatasMobility.java
+++ src/main/tasks/pt/ist/fenix/webapp/FixWronglyCreatedRegistrationDatasMobility.java
@@ -1,7 +1,7 @@
 package pt.ist.fenix.webapp;
 
 import org.fenixedu.academic.domain.ExecutionYear;
-import org.fenixedu.academic.domain.candidacy.IngressionType;
+import org.fenixedu.academic.domain.student.RegistrationProtocol;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
 import pt.ist.fenixframework.FenixFramework;
@@ -9,10 +9,10 @@ import pt.ist.fenixframework.FenixFramework;
 public class FixWronglyCreatedRegistrationDatasMobility extends CustomTask {
     @Override
     public void runTask() throws Exception {
-        final IngressionType mobility = FenixFramework.getDomainObject("5295694675972");
+        final RegistrationProtocol mobility = FenixFramework.getDomainObject("5295694675972");
         final ExecutionYear currentYear = ExecutionYear.readCurrentExecutionYear();
         Bennu.getInstance().getRegistrationDataByExecutionYearSet().stream()
-                .filter(rd -> rd.getRegistration().getIngressionType() == mobility)
+                .filter(rd -> rd.getRegistration().getRegistrationProtocol() == mobility)
                 .filter(rd -> rd.getExecutionYear() == currentYear)
                 .filter(rd -> rd.getRegistration().getStartExecutionYear() == currentYear.getNextExecutionYear())
                 .forEach(rd -> taskLog("%s\t%s%n", rd.getRegistration().getNumber(), rd.getRegistration().getDegreeName()));
