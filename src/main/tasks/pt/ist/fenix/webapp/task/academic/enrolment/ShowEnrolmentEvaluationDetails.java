b17957be646373101ddcc3ed48099620b08b13af
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-07-26 13:01:25 +0100

    Run CustomTask ShowEnrolmentEvaluationDetails

---
+++ src/main/tasks/pt/ist/fenix/webapp/task/academic/enrolment/ShowEnrolmentEvaluationDetails.java
@@ -0,0 +1,32 @@
+package pt.ist.fenix.webapp.task.academic.enrolment;
+
+import org.fenixedu.bennu.core.domain.User;
+import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+
+public class ShowEnrolmentEvaluationDetails extends ReadCustomTask {
+
+    @Override
+    public void runTask() throws Exception {
+        User.findByUsername("ist148915").getPerson().getStudent().getRegistrationsSet().forEach(registration -> {
+            taskLog("Registration: %s%n", registration.getDegree().getPresentationName());
+            registration.getStudentCurricularPlanStream().forEach(scp -> {
+                taskLog("   %s%n", scp.getName());
+                scp.getEnrolmentStream().forEach(enrolment -> {
+                    taskLog("      %s%n", enrolment.getName().getContent());
+                    enrolment.getEvaluationsSet().forEach(ee -> {
+                        taskLog("         %s : %s : %s : %s : %s : %s : %s%n",
+                                ee.getExternalId(),
+                                ee.getWhenDateTime(),
+                                ee.getEvaluationSeason().getAcronym().getContent(),
+                                ee.getGradeValue(),
+                                ee.getMarkSheet() == null ? "null" : ee.getMarkSheet().getCreationDateDateTime().toString(),
+                                ee.getRectificationMarkSheet() == null ? "null" : ee.getRectificationMarkSheet().getCreationDateDateTime().toString(),
+                                ee.getPerson().getUsername()
+                        );
+                    });
+                });
+            });
+        });
+    }
+
+}
