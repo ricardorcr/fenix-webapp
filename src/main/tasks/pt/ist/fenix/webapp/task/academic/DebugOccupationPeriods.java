01210d7d615bfdb045bc1c89a4ee0ea75e5abf9f
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-07-26 12:41:29 +0100

    Run CustomTask DebugOccupationPeriods

--- src/main/tasks/pt/ist/fenix/webapp/task/academic/DebugOccupationPeriods.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/academic/DebugOccupationPeriods.java
@@ -1,12 +1,15 @@
 package pt.ist.fenix.webapp.task.academic;
 
+import org.fenixedu.academic.domain.CurricularYearList;
 import org.fenixedu.academic.domain.OccupationPeriodType;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+import org.fenixedu.bennu.scheduler.custom.WriteCustomTask;
 
+import java.util.Collections;
 import java.util.stream.Collectors;
 
-public class DebugOccupationPeriods extends ReadCustomTask {
+public class DebugOccupationPeriods extends WriteCustomTask {
     @Override
     public void runTask() throws Exception {
         Bennu.getInstance().getExecutionYearsSet().stream()
@@ -16,6 +19,9 @@ public class DebugOccupationPeriods extends ReadCustomTask {
                 //.filter(ref -> ref.getSemester().intValue() == executionSemester.getSemester().intValue())
                 //.filter(ref -> overlap(ref.getCurricularYears().getYears(), curricularYears))
                 .filter(ref -> ref.getCurricularYears() == null)
+                .peek(ref -> {
+                    ref.setCurricularYears(new CurricularYearList(Collections.emptyList()));
+                })
                 .map(ref -> ref.getExecutionDegree().getExecutionYear())
                 .collect(Collectors.toSet())
                 .forEach(executionYear -> taskLog("%s%n", executionYear.getName()));
