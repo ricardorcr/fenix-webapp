6c4cbebc327bc6333616a53988dbad6542225bff
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-08-18 13:37:38 +0100

    Run CustomTask SetupGradeConfig

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/SetupGradeConfig.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/SetupGradeConfig.java
@@ -10,10 +10,15 @@ public class SetupGradeConfig extends WriteCustomTask implements RemoteReader {
     @Override
     public void runTask() throws Exception {
         final AdmissionProcess process = FenixFramework.getDomainObject("571432513830959");
-        process.setGradeConfig(string("teachersPromotionGradeConfig.json"));
+        //process.setGradeConfig(string("teachersPromotionGradeConfig.json"));
         //process.setGradeConfig(null);
-        process.setResultsPublished(false);
-        process.setHasAdmissionGranted(false);
+        process.setResultsPublished(true);
+        process.setHasAdmissionGranted(true);
+        process.getAdmissionProcessTargetSet().stream()
+                .flatMap(target -> target.getApplicationSet().stream())
+                .forEach(application -> {
+                    application.setGrade(null);
+                });
     }
 
 }
