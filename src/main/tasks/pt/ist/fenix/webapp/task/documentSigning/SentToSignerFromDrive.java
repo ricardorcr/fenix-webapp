19507dcc135a05eab3a79b6e60146ed979e0ccbf
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-08-23 11:46:26 +0100

    Run CustomTask SentToSignerFromDrive

--- src/main/tasks/pt/ist/fenix/webapp/task/documentSigning/SentToSignerFromDrive.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/documentSigning/SentToSignerFromDrive.java
@@ -3,19 +3,38 @@ package pt.ist.fenix.webapp.task.documentSigning;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.fenixedu.bennu.RegistrationProcessConfiguration;
+import org.fenixedu.bennu.core.rest.JsonBodyReaderWriter;
 import org.fenixedu.bennu.core.util.CoreConfiguration;
 import org.fenixedu.bennu.io.domain.DriveAPIStorage;
 import org.fenixedu.bennu.io.domain.FileSupport;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
 import org.fenixedu.jwt.Tools;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.media.multipart.file.StreamDataBodyPart;
+import org.joda.time.DateTime;
+import pt.ist.registration.process.handler.CandidacySignalHandler;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 public class SentToSignerFromDrive extends ReadCustomTask {
 
     final String USERNAME = "fenix";
     final String SLUG = "assuntos-academicos/uploadsigneraageral";
+    final String SIGNING_QUEUE = "8lr7HhFi";
 
     private String accessToken = accessToken(USERNAME);
 
@@ -28,6 +47,10 @@ public class SentToSignerFromDrive extends ReadCustomTask {
             final String downloadLink = item.get("downloadLink").getAsString();
             final byte[] content = read(downloadLink);
             taskLog("%s = %s bytes%n", name, content.length);
+            final String title = name.replace(".pdf", "");
+            final ByteArrayInputStream stream = new ByteArrayInputStream(content);
+            final String uuid = UUID.randomUUID().toString();
+            sendDocumentToBeSigned(SIGNING_QUEUE, title, title, name, stream, uuid);
         }
     }
 
@@ -66,6 +89,39 @@ public class SentToSignerFromDrive extends ReadCustomTask {
         final JsonObject claim = new JsonObject();
         claim.addProperty("username", username);
         return Tools.sign(SignatureAlgorithm.RS256, CoreConfiguration.getConfiguration().jwtPrivateKeyPath(), claim);
+    }
+
+    public void sendDocumentToBeSigned(final String queue, final String title, final String description,
+                                       final String filename, final InputStream contentStream,
+                                       final String uuid) {
+        final String compactJws = Jwts.builder()
+                .setSubject(RegistrationProcessConfiguration.getConfiguration().signerJwtUser())
+                .setExpiration(DateTime.now().plusHours(6).toDate())
+                .signWith(SignatureAlgorithm.HS512, RegistrationProcessConfiguration.signerJwtSecret()).compact();
+
+        try (final FormDataMultiPart formDataMultiPart = new FormDataMultiPart()) {
+            final StreamDataBodyPart streamDataBodyPart = new StreamDataBodyPart("file", contentStream, filename, new MediaType("application", "pdf"));
+            formDataMultiPart.bodyPart(streamDataBodyPart);
+            formDataMultiPart.bodyPart(new FormDataBodyPart("queue", queue));
+            formDataMultiPart.bodyPart(new FormDataBodyPart("creator", "Sistema FenixEdu"));
+            formDataMultiPart.bodyPart(new FormDataBodyPart("filename", filename));
+            formDataMultiPart.bodyPart(new FormDataBodyPart("title", title));
+            formDataMultiPart.bodyPart(new FormDataBodyPart("description", description));
+            formDataMultiPart.bodyPart(new FormDataBodyPart("externalIdentifier", uuid));
+            formDataMultiPart.bodyPart(new FormDataBodyPart("signatureField", CandidacySignalHandler.SIGNATURE_FIELD));
+
+//            final String nounce = Jwts.builder().setSubject(uuid).signWith(SignatureAlgorithm.HS512, RegistrationProcessConfiguration.signerJwtSecret()).compact();
+//            formDataMultiPart.bodyPart(new FormDataBodyPart("callbackUrl", CoreConfiguration.getConfiguration().applicationUrl()
+//                    + "/adhock-document/store/" + username + "/" + filename + "?nounce=" + nounce));
+            final Client client = ClientBuilder.newClient();
+            client.register(MultiPartFeature.class);
+            client.register(JsonBodyReaderWriter.class);
+            client.target(RegistrationProcessConfiguration.getConfiguration().signerUrl()).path("sign-requests")
+                    .request().header("Authorization", "Bearer " + compactJws)
+                    .post(Entity.entity(formDataMultiPart, MediaType.MULTIPART_FORM_DATA_TYPE), String.class);
+        } catch (final IOException e) {
+            throw new Error(e);
+        }
     }
 
 }
