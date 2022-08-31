e48057ccf06c46c86e1fd3c83e210002c1564871
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-08-31 11:03:53 +0100

    Run CustomTask ShowEnrolmentEvaluationDetails

--- src/main/tasks/pt/ist/fenix/webapp/task/academic/enrolment/ShowEnrolmentEvaluationDetails.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/academic/enrolment/ShowEnrolmentEvaluationDetails.java
@@ -7,12 +7,18 @@ public class ShowEnrolmentEvaluationDetails extends ReadCustomTask {
 
     @Override
     public void runTask() throws Exception {
-        User.findByUsername("ist148915").getPerson().getStudent().getRegistrationsSet().forEach(registration -> {
+        User.findByUsername("ist195692").getPerson().getStudent().getRegistrationsSet().forEach(registration -> {
             taskLog("Registration: %s%n", registration.getDegree().getPresentationName());
             registration.getStudentCurricularPlanStream().forEach(scp -> {
                 taskLog("   %s%n", scp.getName());
                 scp.getEnrolmentStream().forEach(enrolment -> {
                     taskLog("      %s%n", enrolment.getName().getContent());
+                    taskLog("         peso: %s : peso curriculo: %s : ects: %s ecto curriculo: %s%n",
+                            enrolment.getWeigth(),
+                            enrolment.getWeigthForCurriculum(),
+                            enrolment.getEctsCredits(),
+                            enrolment.getEctsCreditsForCurriculum()
+                            );
                     enrolment.getEvaluationsSet().forEach(ee -> {
                         taskLog("         %s : %s : %s : %s : %s : %s : %s%n",
                                 ee.getExternalId(),
