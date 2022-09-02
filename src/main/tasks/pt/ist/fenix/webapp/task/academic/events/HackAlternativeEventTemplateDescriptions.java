fc7c83f611381536828a620ef33f3f51b70701f9
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-02 14:03:38 +0100

    Run CustomTask HackAlternativeEventTemplateDescriptions

--- src/main/tasks/pt/ist/fenix/webapp/task/academic/events/HackAlternativeEventTemplateDescriptions.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/academic/events/HackAlternativeEventTemplateDescriptions.java
@@ -2,6 +2,7 @@ package pt.ist.fenix.webapp.task.academic.events;
 
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+import org.fenixedu.commons.i18n.LocalizedString;
 
 import java.util.Locale;
 
@@ -13,11 +14,26 @@ public class HackAlternativeEventTemplateDescriptions extends ReadCustomTask {
     @Override
     public void runTask() throws Exception {
         Bennu.getInstance().getEventTemplateSet().forEach(eventTemplate -> {
+            final LocalizedString title = eventTemplate.getTitle();
+            final LocalizedString description = eventTemplate.getDescription()
+                    .map(s -> s.replace(
+                            "limita as inscrições a um máximo de 42.0 ECTS",
+                            "limita as inscrições a um máximo de 70% dos ECTS permitidos para o regime normal"))
+                    .map(s -> s.replace(
+                            "limits enrolments to a maximum of 42.0 ECTS",
+                            "limits enrolments to a maximum of 70% of ECTS allowed for the normal regime."))
+                    .map(s -> s.replace(
+                            "limita as inscrições a um máximo de 30.0 ECTS",
+                            "limita as inscrições a um máximo de 50% dos ECTS permitidos para o regime normal"))
+                    .map(s -> s.replace(
+                            "limits enrolments to a maximum of 30.0 ECTS",
+                            "limits enrolments to a maximum of 50% of ECTS allowed for the normal regime."))
+                    ;
             taskLog("%s%n%s%n%n%s%n%s%n%n%n%n",
-                    eventTemplate.getTitle().getContent(PT),
-                    eventTemplate.getTitle().getContent(EN),
-                    eventTemplate.getDescription().getContent(PT),
-                    eventTemplate.getDescription().getContent(EN));
+                    title.getContent(PT),
+                    title.getContent(EN),
+                    description.getContent(PT),
+                    description.getContent(EN));
         });
     }
 
