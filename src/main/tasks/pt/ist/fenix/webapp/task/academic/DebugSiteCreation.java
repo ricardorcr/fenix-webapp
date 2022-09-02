212abb1e7783b219422c655098d8ae4ad4e6c307
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-02 13:18:46 +0100

    Run CustomTask DebugSiteCreation

--- src/main/tasks/pt/ist/fenix/webapp/task/academic/DebugSiteCreation.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/academic/DebugSiteCreation.java
@@ -4,10 +4,15 @@ import org.fenixedu.academic.domain.Degree;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
 
 import java.lang.ref.SoftReference;
+import java.lang.reflect.Method;
 
 public class DebugSiteCreation extends ReadCustomTask {
     @Override
     public void runTask() throws Exception {
+        final Method method = Degree.class.getDeclaredMethod("loadCache");
+        method.setAccessible(true);
+        method.invoke(null);
+
         final Degree degree = Degree.readBySigla("Min-EG");
         taskLog("Degree = %s%n", degree);
 
