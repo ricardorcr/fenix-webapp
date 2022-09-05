19b7c6d8acc3803cbe14b14f8ab0b154717ca91a
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-05 11:22:25 +0100

    Run CustomTask DebugEventTemplates

--- src/main/tasks/pt/ist/fenix/webapp/task/academic/DebugEventTemplates.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/academic/DebugEventTemplates.java
@@ -4,21 +4,24 @@ import org.fenixedu.academic.domain.accounting.EventTemplate;
 import org.fenixedu.academic.domain.accounting.EventTemplateConfig;
 import org.fenixedu.academic.domain.student.Registration;
 import org.fenixedu.academic.domain.student.RegistrationDataByExecutionYear;
+import org.fenixedu.academic.domain.student.Student;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
 
 public class DebugEventTemplates extends ReadCustomTask {
     @Override
     public void runTask() throws Exception {
-        final Registration registration = Registration.readByNumber(105312).iterator().next();
-        final EventTemplate eventTemplate = registration.getEventTemplate();
-        for (final RegistrationDataByExecutionYear dataByYear : registration.getRegistrationDataByExecutionYearSet()) {
-            EventTemplate eventTemplateForYear = dataByYear.getEventTemplate();
-            if (eventTemplateForYear == null) {
-                eventTemplateForYear = eventTemplate;
-            }
-            final EventTemplateConfig templateConfig = eventTemplateForYear.getConfigFor(dataByYear.getExecutionYear()
-                    .getBeginLocalDate().plusDays(12).toDateTimeAtStartOfDay());
+        final Student student = Student.readStudentByNumber(96542);
+        for (final Registration registration : student.getRegistrationsSet()) {
+            final EventTemplate eventTemplate = registration.getEventTemplate();
+            for (final RegistrationDataByExecutionYear dataByYear : registration.getRegistrationDataByExecutionYearSet()) {
+                EventTemplate eventTemplateForYear = dataByYear.getEventTemplate();
+                if (eventTemplateForYear == null) {
+                    eventTemplateForYear = eventTemplate;
+                }
+                final EventTemplateConfig templateConfig = eventTemplateForYear.getConfigFor(dataByYear.getExecutionYear()
+                        .getBeginLocalDate().plusDays(12).toDateTimeAtStartOfDay());
 
+            }
         }
     }
 }
