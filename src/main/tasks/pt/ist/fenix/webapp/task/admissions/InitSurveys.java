6547b52bf0055329517f3ef394ffa4bd2cff81a5
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-06 10:45:41 +0100

    Run CustomTask InitSurveys

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/InitSurveys.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/InitSurveys.java
@@ -1,15 +1,44 @@
 package pt.ist.fenix.webapp.task.admissions;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.fenixedu.bennu.LimeSurveySDKConfiguration;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
 import org.fenixedu.collaboration.limesurvey.LimeSurveyClient;
 
 import java.io.IOException;
+import java.util.UUID;
+import java.util.function.Consumer;
 
 public class InitSurveys extends ReadCustomTask {
 
+    private String basicRemoteCall(final String method, final Consumer<JsonArray> paramSetter) {
+        final JsonObject body = new JsonObject();
+        final JsonArray params = new JsonArray();
+        body.addProperty("jsonrpc", "2.0");
+        body.addProperty("method", method);
+        body.add("params", params);
+        paramSetter.accept(params);
+        body.addProperty("id", UUID.randomUUID().toString());
+
+        final String url = "https://surveys.tecnico.ulisboa.pt/index.php?r=admin/remotecontrol";
+        final HttpResponse<String> response = Unirest.post(url).header("Content-Type", "application/json").body(body).asString();
+
+        taskLog("code: %s%n", response.getStatus());
+        taskLog("response: %s%n", response.getBody());
+        return response.getBody();
+    }
+
     @Override
     public void runTask() throws Exception {
+        basicRemoteCall("get_session_key", params -> {
+            params.add(LimeSurveySDKConfiguration.getConfiguration().limeSurveyUsername());
+            params.add(LimeSurveySDKConfiguration.getConfiguration().limeSurveyPassword());
+        });
+        
         final int id1 = 918456;
         final int id2 = 865334;
         try (final LimeSurveyClient client = new LimeSurveyClient()) {
