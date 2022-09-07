e2c67966b1c43f30eaaa55b886bdb54f83afd179
Ricardo da Conceição Rodrigues <ricardo.rodrigues@tecnico.ulisboa.pt>
2022-09-07 15:26:27 +0100

    Run CustomTask CheckShouldBeAbandoned

--- src/main/tasks/pt/ist/fenix/webapp/CheckShouldBeAbandoned.java
+++ src/main/tasks/pt/ist/fenix/webapp/CheckShouldBeAbandoned.java
@@ -1,50 +1,60 @@
 package pt.ist.fenix.webapp;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import org.fenixedu.academic.domain.DegreeCurricularPlan;
 import org.fenixedu.academic.domain.Enrolment;
 import org.fenixedu.academic.domain.ExecutionSemester;
 import org.fenixedu.academic.domain.ExecutionYear;
 import org.fenixedu.academic.domain.StudentCurricularPlan;
-import org.fenixedu.academic.domain.degreeStructure.CycleType;
+import org.fenixedu.academic.domain.administrativeOffice.AdministrativeOfficeType;
 import org.fenixedu.academic.domain.student.Registration;
-import org.fenixedu.academic.domain.student.RegistrationProtocol;
 import org.fenixedu.academic.domain.student.registrationStates.RegistrationState;
 import org.fenixedu.academic.domain.student.registrationStates.RegistrationStateType;
 import org.fenixedu.academic.domain.studentCurriculum.CurriculumLine;
 import org.fenixedu.academic.domain.studentCurriculum.CurriculumModule;
-import org.fenixedu.academic.domain.studentCurriculum.CycleCurriculumGroup;
 import org.fenixedu.academic.util.predicates.AndPredicate;
-import org.fenixedu.admissions.domain.AdmissionProcess;
-import org.fenixedu.admissions.domain.AdmissionProcessTarget;
-import org.fenixedu.admissions.domain.AdmissionsSystem;
-import org.fenixedu.admissions.domain.Application;
-import org.fenixedu.admissions.ist.domain.Utils;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
 import org.joda.time.YearMonthDay;
 import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
-import pt.ist.fenixedu.integration.ui.struts.action.academicAdministration.UpdateAbandonStateBean;
-import pt.ist.fenixframework.DomainObject;
 import pt.ist.fenixframework.FenixFramework;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.function.Predicate;
 
 public class CheckShouldBeAbandoned extends CustomTask {
 
     private static final String RESOURCE_BUNDLE = "FENIXEDU_IST_INTEGRATION_RESOURCES";
-  
+
     @Override
     public void runTask() throws Exception {
 
-        final ExecutionSemester second2021_2022 = ExecutionYear.readCurrentExecutionYear().getLastExecutionPeriod();
-        final Registration registration = FenixFramework.getDomainObject("283734129518171");
-        final boolean result = evaluateAndCheckEnrolments(registration, second2021_2022);
-        taskLog("Devia estar: %s%n", !result);
+        int changedForAbandoned = 0;
+        for (final DegreeCurricularPlan degreeCurricularPlan : DegreeCurricularPlan.readBolonhaDegreeCurricularPlans()) {
+            if (degreeCurricularPlan.getDegree().getAdministrativeOffice().getAdministrativeOfficeType() != AdministrativeOfficeType.DEGREE) {
+                continue;
+            }
+            if (degreeCurricularPlan.hasExecutionDegreeFor(getWhenToAbandon().getExecutionYear())
+                    || degreeCurricularPlan.hasExecutionDegreeFor(getWhenToAbandon().getExecutionYear()
+                    .getPreviousExecutionYear())) {
 
-        final boolean processed = processStudent(registration.getActiveStudentCurricularPlan());
-        taskLog("Processado com sucesso: %s%n", processed);
+                for (final StudentCurricularPlan scp : degreeCurricularPlan.getActiveStudentCurricularPlans()) {
+                    try {
+                        processStudent(scp);
+                        changedForAbandoned++;
+                    } catch (Exception e) {
+                       taskLog("Aluno %s  - %s devia estar em abandono%n", scp.getPerson().getUsername(), scp.getDegreeCurricularPlan().getName());
+                       e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        taskLog("Estados mudados: %s%n", changedForAbandoned);
+//        final ExecutionSemester second2021_2022 = ExecutionYear.readCurrentExecutionYear().getLastExecutionPeriod();
+//        final Registration registration = FenixFramework.getDomainObject("283734129518171");
+//        final boolean result = evaluateAndCheckEnrolments(registration, second2021_2022);
+//        taskLog("Devia estar: %s%n", !result);
+//
+//        final boolean processed = processStudent(registration.getActiveStudentCurricularPlan());
+//        taskLog("Processado com sucesso: %s%n", processed);
     }
 
     private ExecutionSemester getWhenToAbandon() {
@@ -108,14 +118,14 @@ public class CheckShouldBeAbandoned extends CustomTask {
         if (hasValidRegistrationAgreement(registration) && registration.isDegreeAdministrativeOffice()
                 && lastRegistrationState != null && lastRegistrationState.isActive()
                 && !lastRegistrationState.getStateType().equals(RegistrationStateType.MOBILITY) && !registration.hasConcluded()) {
-            taskLog("Primeiro If");
+//            taskLog("Primeiro If");
             if (registration.getStartExecutionYear().isBefore(getWhenToAbandon().getExecutionYear())
                     && !hasAnyEnrolmentInPeriodOrPrevious(registration, startChecking)) {
-                taskLog("Segundo If");
+//                taskLog("Segundo If");
                 if (registration.hasStateType(getWhenToAbandon().getPreviousExecutionPeriod(),
                         RegistrationStateType.EXTERNAL_ABANDON)
                         || registration.hasStateType(getWhenToAbandon(), RegistrationStateType.EXTERNAL_ABANDON)) {
-                    taskLog("Terceiro If");
+//                    taskLog("Terceiro If");
                     return false;
                 }
                 final YearMonthDay now = new YearMonthDay();
@@ -130,7 +140,7 @@ public class CheckShouldBeAbandoned extends CustomTask {
                 return true;
             }
         }
-        taskLog("Não entrou");
+//        taskLog("Não entrou");
         return false;
     }
 
