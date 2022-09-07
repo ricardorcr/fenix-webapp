dfd3b095c0fc3ee315a9a2156b3d6a25a96fe80e
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-07 16:26:38 +0100

    Run CustomTask InitConfirmationSlots

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/InitConfirmationSlots.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/InitConfirmationSlots.java
@@ -5,6 +5,7 @@ import org.fenixedu.academic.domain.degreeStructure.CycleType;
 import org.fenixedu.admissions.domain.AdmissionProcess;
 import org.fenixedu.admissions.domain.AdmissionProcessTarget;
 import org.fenixedu.admissions.domain.AdmissionsSystem;
+import org.fenixedu.admissions.domain.Application;
 import org.fenixedu.admissions.ist.domain.Utils;
 import org.fenixedu.bennu.core.json.JsonUtils;
 import org.fenixedu.bennu.scheduler.custom.WriteCustomTask;
@@ -19,6 +20,7 @@ public class InitConfirmationSlots extends WriteCustomTask {
                 .filter(this::needToApplySurvey)
                 .flatMap(admissionProcess -> admissionProcess.getAdmissionProcessTargetSet().stream())
                 .filter(this::init)
+                .filter(this::cycle)
                 .flatMap(admissionProcessTarget -> admissionProcessTarget.getApplicationSet().stream())
                 .filter(application -> application.getLockInstant() != null)
                 .filter(application -> Utils.registrationFor(application) != null)
@@ -46,6 +48,12 @@ public class InitConfirmationSlots extends WriteCustomTask {
         ;
 
         taskLog("Need %s slots%n", slots);
+    }
+
+    private boolean cycle(final AdmissionProcessTarget target) {
+        final JsonObject config = target.getOutcomeConfigJson();
+        final CycleType cycleType = cycleTypeFor(config);
+        return cycleType == CycleType.SECOND_CYCLE;
     }
 
     private boolean init(final AdmissionProcessTarget admissionProcessTarget) {
