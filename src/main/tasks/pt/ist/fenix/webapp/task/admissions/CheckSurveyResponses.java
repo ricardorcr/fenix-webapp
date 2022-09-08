8d91e46b52833870f326b4f1b2d65d896aee2efd
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-08 15:01:17 +0100

    Run CustomTask CheckSurveyResponses

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/CheckSurveyResponses.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/CheckSurveyResponses.java
@@ -36,15 +36,8 @@ public class CheckSurveyResponses extends ReadCustomTask implements RemoteReader
                             : cycleType == CycleType.SECOND_CYCLE ? surveyCycle2
                             : null;
                     if (survey != null) {
-/*
                         taskLog("app = %s%n", application.getAccount().getEmail());
                         add(application, survey);
-
- */
-                        Survey.surveys(application).forEach(s -> {
-                            taskLog("Application: %s%n", application.getExternalId());
-                            taskLog("   %s%n", s.toString());
-                        });
                     }
                 });
         ;
