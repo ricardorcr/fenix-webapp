f68e4e0c079529ce09826a0cb203061cdaf5d4d4
Ricardo da Conceição Rodrigues <ricardo.rodrigues@tecnico.ulisboa.pt>
2022-09-08 17:36:44 +0100

    Run CustomTask CheckAdmissionsOutcomeState

--- src/main/tasks/pt/ist/fenix/webapp/CheckAdmissionsOutcomeState.java
+++ src/main/tasks/pt/ist/fenix/webapp/CheckAdmissionsOutcomeState.java
@@ -84,8 +84,8 @@ public class CheckAdmissionsOutcomeState extends CustomTask {
                 if (Utils.isToChangeOutcomeState(admissionProcess)) {
                     if (Utils.needsDocumentConfirmation(admissionProcess)) {
                         taskLog("Should change for REGISTERED - %s - %s%n", application.getExternalId(), application.getAdmissionProcessTarget().getAdmissionProcess().getTitle().getContent());
-                       // RegistrationService.setOutcomeState(application, RegistrationProcessState.REGISTERED);
-                       // RegistrationService.addToConfirmationQueueIfNeeded(application);                                                                               
+                        RegistrationService.setOutcomeState(application, RegistrationProcessState.REGISTERED);
+                        RegistrationService.addToConfirmationQueueIfNeeded(application);                                                                               
                     } else {
                         taskLog("Should change for CONFIRMED - %s%n", application.getExternalId(), application.getAdmissionProcessTarget().getAdmissionProcess().getTitle().getContent());
                     }
