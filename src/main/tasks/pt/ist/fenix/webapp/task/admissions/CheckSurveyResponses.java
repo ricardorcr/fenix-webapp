cc9a60e068e14a7f5fc64e69aca64def43e7b982
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-08 14:24:16 +0100

    Run CustomTask CheckSurveyResponses

---
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/CheckSurveyResponses.java
@@ -0,0 +1,99 @@
+package pt.ist.fenix.webapp.task.admissions;
+
+import com.google.gson.JsonObject;
+import org.fenixedu.academic.domain.degreeStructure.CycleType;
+import org.fenixedu.admissions.domain.AdmissionProcess;
+import org.fenixedu.admissions.domain.AdmissionProcessTarget;
+import org.fenixedu.admissions.domain.AdmissionsSystem;
+import org.fenixedu.admissions.domain.Application;
+import org.fenixedu.admissions.ist.domain.Survey;
+import org.fenixedu.admissions.ist.domain.Utils;
+import org.fenixedu.admissions.util.RemoteReader;
+import org.fenixedu.bennu.core.json.JsonUtils;
+import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+import pt.ist.fenixframework.FenixFramework;
+
+public class CheckSurveyResponses extends ReadCustomTask implements RemoteReader {
+
+    final JsonObject surveyCycle1 = object("cycle1.json");
+    final JsonObject surveyCycle2 = object("cycle2.json");
+
+    @Override
+    public void runTask() throws Exception {
+
+        AdmissionsSystem.getInstance().getAdmissionProcessSet().stream()
+                .filter(this::needToApplySurvey)
+                .flatMap(admissionProcess -> admissionProcess.getAdmissionProcessTargetSet().stream())
+                .filter(this::init)
+                .flatMap(admissionProcessTarget -> admissionProcessTarget.getApplicationSet().stream())
+                .filter(application -> application.getLockInstant() != null)
+                .filter(application -> Utils.registrationFor(application) != null)
+                .forEach(application -> {
+                    final AdmissionProcessTarget admissionProcessTarget = application.getAdmissionProcessTarget();
+                    final JsonObject config = admissionProcessTarget.getOutcomeConfigJson();
+                    final CycleType cycleType = cycleTypeFor(config);
+                    final JsonObject survey = cycleType == CycleType.FIRST_CYCLE ? surveyCycle1
+                            : cycleType == CycleType.SECOND_CYCLE ? surveyCycle2
+                            : null;
+                    if (survey != null) {
+                        taskLog("app = %s%n", application.getAccount().getEmail());
+                        add(application, survey);
+                    }
+                });
+        ;
+    }
+
+    private void add(final Application application, final JsonObject survey) {
+        try {
+            FenixFramework.atomic(() -> {
+                if (Survey.survey(application, survey.get("surveyId").getAsString()) == null) {
+                    taskLog("Add survey for: " + application.getExternalId());
+                    Survey.addSurvey(application, survey);
+                } else {
+                    taskLog("Check survey response for: " + application.getExternalId());
+                    Survey.updateResponseStatus(application);
+                }
+            });
+        } catch (final Throwable t) {
+            taskLog("   Failled to add survey to %s%n", application.getExternalId());
+        }
+    }
+
+    private boolean init(final AdmissionProcessTarget admissionProcessTarget) {
+        final JsonObject config = admissionProcessTarget.getOutcomeConfigJson();
+        final CycleType cycleType = cycleTypeFor(config);
+        final JsonObject survey = cycleType == CycleType.FIRST_CYCLE ? surveyCycle1
+                : cycleType == CycleType.SECOND_CYCLE ? surveyCycle2
+                : null;
+        if (survey == null) {
+            taskLog("Unable to init: %s > %s%n",
+                    admissionProcessTarget.getAdmissionProcess().getTitle().getContent(),
+                    admissionProcessTarget.getName().getContent());
+            return false;
+        } else {
+            config.add("surveyConcludeBoarding", survey);
+            //admissionProcessTarget.setOutcomeConfig(config.toString());
+            return true;
+        }
+    }
+
+    private boolean needToApplySurvey(final AdmissionProcess admissionProcess) {
+        return admissionProcess.getTitle().getContent().indexOf("2023") > 0 && (
+                Utils.isDegreeType(admissionProcess)
+                || Utils.isDegreeSpecificRegimentType(admissionProcess)
+                || Utils.isReinstatement(admissionProcess)
+                || Utils.isDges(admissionProcess)
+                );
+    }
+
+    private CycleType cycleTypeFor(final JsonObject config) {
+        final String cycleType = JsonUtils.get(config, "cycleType");
+        return cycleType == null ? null : CycleType.valueOf(cycleType);
+    }
+
+    @Override
+    public String baseUrl() {
+        return "https://repo.dsi.tecnico.ulisboa.pt/fenixedu/data/-/raw/master/admissions/surveys/";
+    }
+
+}
