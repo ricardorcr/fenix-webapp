d1fb10e25a195a0e01387af79582fd891928e148
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-08 14:48:22 +0100

    Run CustomTask ForceSurveyUpdate

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/ForceSurveyUpdate.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/ForceSurveyUpdate.java
@@ -1,14 +1,47 @@
 package pt.ist.fenix.webapp.task.admissions;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.fenixedu.admissions.domain.Application;
 import org.fenixedu.admissions.ist.domain.Survey;
+import org.fenixedu.bennu.core.util.CoreConfiguration;
 import org.fenixedu.bennu.scheduler.custom.WriteCustomTask;
+import org.fenixedu.collaboration.limesurvey.LimeSurveySDK;
+import org.fenixedu.commons.stream.StreamUtils;
+import org.joda.time.DateTime;
 import pt.ist.fenixframework.FenixFramework;
 
 public class ForceSurveyUpdate extends WriteCustomTask {
     @Override
     public void runTask() throws Exception {
         final Application application = FenixFramework.getDomainObject("571415333968005");
+        Survey.surveys(application)
+                .filter(Survey::pendingResponse)
+                .filter(survey -> shouldMarkAsCompleted(survey))
+                .forEach(survey -> markAsCompleted(application, Survey.id(survey)));
+
         Survey.updateResponseStatus(application);
     }
+
+    private boolean shouldMarkAsCompleted(final JsonObject survey) {
+        if (CoreConfiguration.getConfiguration().developmentMode()) {
+            final DateTime now = new DateTime();
+            final String min = Integer.toString(now.getMinuteOfHour());
+            return min.endsWith("1") || min.endsWith("3") || min.endsWith("5") || min.endsWith("7") || min.endsWith("9");
+        }
+        taskLog("Should " + Survey.email(survey) + " " + Survey.id(survey));
+        return LimeSurveySDK.hasCompletedSurvey(Survey.id(survey), Survey.email(survey));
+    }
+
+    private void markAsCompleted(final Application application, final String surveyId) {
+        taskLog("Makr");
+        final JsonObject data = application.getDataObject();
+        final JsonArray surveys = data.getAsJsonArray("surveys");
+        StreamUtils.of(surveys).map(e -> e.getAsJsonObject())
+                .filter(survey -> surveyId.equals(Survey.id(survey)))
+                .forEach(survey -> survey.addProperty("completed", new DateTime().toString("yyyy-MM-dd HH:mm:ss")));
+        application.setData(data.toString());
+        //Signal.emit(SURVEY_COMPLETED, new DomainObjectEvent<>(application));
+    }
+
 }
