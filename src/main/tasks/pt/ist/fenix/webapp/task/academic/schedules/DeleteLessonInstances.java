aac9bc9a84dd323a91ac4836e955cd823f56df72
Susana Luísa Da Silveira Pires Fernandes <susana.fernandes@tecnico.ulisboa.pt>
2022-09-08 10:23:38 +0100

    Run CustomTask DeleteLessonInstances

--- src/main/tasks/pt/ist/fenix/webapp/task/academic/schedules/DeleteLessonInstances.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/academic/schedules/DeleteLessonInstances.java
@@ -3,6 +3,7 @@ package pt.ist.fenix.webapp.task.academic.schedules;
 import java.io.ByteArrayOutputStream;
 
 import org.fenixedu.academic.domain.ExecutionSemester;
+import org.fenixedu.academic.domain.LessonInstance;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
@@ -13,27 +14,30 @@ public class DeleteLessonInstances extends CustomTask {
 
     @Override
     public void runTask() throws Exception {
-        final Spreadsheet spreadsheet = new Spreadsheet("InstanciasDia23");
+        final Spreadsheet spreadsheet = new Spreadsheet("InstanciasForaPeriodo");
         
-        LocalDate date = new LocalDate(2022, 12, 23);
         ExecutionSemester executionSemester = ExecutionSemester.readActualExecutionSemester().getNextExecutionPeriod();
         taskLog(executionSemester.getQualifiedName());
 
         executionSemester.getAssociatedExecutionCoursesSet().stream().flatMap(ec -> ec.getLessons().stream())
-                .flatMap(l -> l.getLessonInstancesSet().stream()).filter(li -> li.getDay().toLocalDate().equals(date))
+                .flatMap(l -> l.getLessonInstancesSet().stream()).filter(li -> !isInLessonPeriod(li))
                 .forEach(li -> {
                     final Spreadsheet.Row row = spreadsheet.addRow();
                     row.setCell("Curso", li.getLesson().getExecutionCourse().getDegreePresentationString());
                     row.setCell("UC",  li.getLesson().getExecutionCourse().getName());
                     row.setCell("Turno",  li.getLesson().getShift().getNome());
                     row.setCell("Instância",  li.getDay().toString());
-                    li.delete();
+                 //   li.delete();
                 });
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         spreadsheet.exportToXLSSheet(baos);
-        output("instanciasDia23.xls", baos.toByteArray());
+        output("instanciasForaPeriodo.xls", baos.toByteArray());
         taskLog("\nDone");
+    }
+
+    private boolean isInLessonPeriod(LessonInstance li) {
+        return li.getLesson().getExecutionCourse().getLessonOccupationPeriod().nestedOccupationPeriodsContainsDay(li.getDay());
     }
 
 }
