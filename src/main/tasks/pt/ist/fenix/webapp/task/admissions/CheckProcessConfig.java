5edec39a51fc7b1b105355d32e905a59518528e5
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-08 15:44:14 +0100

    Run CustomTask CheckProcessConfig

---
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/CheckProcessConfig.java
@@ -0,0 +1,37 @@
+package pt.ist.fenix.webapp.task.admissions;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.fenixedu.admissions.domain.AdmissionProcess;
+import org.fenixedu.admissions.domain.AdmissionsSystem;
+import org.fenixedu.admissions.ist.domain.Utils;
+import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+
+public class CheckProcessConfig extends ReadCustomTask {
+
+    @Override
+    public void runTask() throws Exception {
+        AdmissionsSystem.getInstance().getAdmissionProcessSet().stream()
+                .filter(this::include)
+                .forEach(admissionProcess -> {
+                    final JsonObject config = admissionProcess.getOutcomeConfigJson();
+                    final JsonElement needsDocumentsConfirmation = config.get("needsDocumentsConfirmation");
+                    final JsonElement automaticEnrollment = config.get("automaticEnrollment");
+                    final JsonElement tutorDistribution = config.get("tutorDistribution");
+                    taskLog("%s: %s%n   %s%n   %s%n   %s%n",
+                            admissionProcess.getExternalId(),
+                            admissionProcess.getTitle().getContent(),
+                            needsDocumentsConfirmation != null && needsDocumentsConfirmation.getAsBoolean(),
+                            automaticEnrollment != null && automaticEnrollment.getAsBoolean(),
+                            tutorDistribution != null && tutorDistribution.getAsBoolean());
+                });
+    }
+
+    private boolean include(final AdmissionProcess admissionProcess) {
+        return admissionProcess.getTitle().getContent().indexOf("2023") > 0
+                && !Utils.isMinor(admissionProcess)
+                && !Utils.isHACS(admissionProcess)
+                && !Utils.isOutboundMobilityType(admissionProcess);
+    }
+
+}
