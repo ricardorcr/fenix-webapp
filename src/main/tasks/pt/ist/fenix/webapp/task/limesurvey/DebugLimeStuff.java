9ee2b58a7c71746334b8e97d207220907244a393
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-09 15:17:02 +0100

    Run CustomTask DebugLimeStuff

---
+++ src/main/tasks/pt/ist/fenix/webapp/task/limesurvey/DebugLimeStuff.java
@@ -0,0 +1,24 @@
+package pt.ist.fenix.webapp.task.limesurvey;
+
+import org.fenixedu.admissions.ist.domain.Survey;
+import org.fenixedu.bennu.core.domain.User;
+import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+import org.fenixedu.connect.pluggable.PluggableActivity;
+
+public class DebugLimeStuff extends ReadCustomTask {
+
+    @Override
+    public void runTask() throws Exception {
+        final User user = User.findByUsername("ist1105708");
+        user.getIdentity().getAccountSet().stream()
+                .flatMap(a -> a.getApplicationSet().stream())
+                .peek(application -> Survey.updateResponseStatus(application))
+                .flatMap(application -> Survey.surveys(application))
+                .filter(survey -> Survey.pendingResponse(survey))
+                .forEach(survey -> {
+                    taskLog("%s : %s%n", Survey.id(survey), Survey.email(survey));
+                });
+
+    }
+
+}
