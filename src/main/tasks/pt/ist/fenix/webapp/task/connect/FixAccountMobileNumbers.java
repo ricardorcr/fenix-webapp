35dcae3e807205dfdae33b968bde5cdebddbf95a
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-09 18:09:29 +0100

    Run CustomTask FixAccountMobileNumbers

--- src/main/tasks/pt/ist/fenix/webapp/task/connect/FixAccountMobileNumbers.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/connect/FixAccountMobileNumbers.java
@@ -5,6 +5,7 @@ import org.fenixedu.connect.domain.Account;
 import org.fenixedu.connect.domain.ConnectSystem;
 import org.fenixedu.connect.domain.Identity;
 import org.fenixedu.connect.domain.identification.PersonalInformation;
+import pt.ist.fenixframework.FenixFramework;
 import pt.ist.standards.telecommunications.DailingCode;
 
 public class FixAccountMobileNumbers extends ReadCustomTask {
@@ -14,31 +15,33 @@ public class FixAccountMobileNumbers extends ReadCustomTask {
     }
 
     private void fix(final Account account) {
-        final String mobile = account.getMobile();
-        if (mobile != null) {
-            final Identity identity = account.getIdentity();
-            if (identity != null) {
-                final PersonalInformation personalInformation = identity.getPersonalInformation();
-                if (personalInformation != null) {
-                    final String countryCode = personalInformation.getNationalityCountryCode();
-                    if (countryCode != null) {
-                        final String prefix = DailingCode.dailingPrefixFor(countryCode);
-                        if (mobile.startsWith(prefix)) {
-                            final String numberString = mobile.substring(prefix.length());
-                            final int number = Integer.parseInt(numberString);
-                            final String fix = prefix + number;
-                            if (!mobile.equals(fix)) {
-                                taskLog("Fixing account: %s : %s -> %s %s%n",
-                                        account.getEmail(),
-                                        mobile,
-                                        prefix,
-                                        number);
+        FenixFramework.atomic(() -> {
+            final String mobile = account.getMobile();
+            if (mobile != null) {
+                final Identity identity = account.getIdentity();
+                if (identity != null) {
+                    final PersonalInformation personalInformation = identity.getPersonalInformation();
+                    if (personalInformation != null) {
+                        final String countryCode = personalInformation.getNationalityCountryCode();
+                        if (countryCode != null) {
+                            final String prefix = DailingCode.dailingPrefixFor(countryCode);
+                            if (mobile.startsWith(prefix)) {
+                                final String numberString = mobile.substring(prefix.length());
+                                final int number = Integer.parseInt(numberString);
+                                final String fix = prefix + number;
+                                if (!mobile.equals(fix)) {
+                                    taskLog("Fixing account: %s : %s -> %s %s%n",
+                                            account.getEmail(),
+                                            mobile,
+                                            prefix,
+                                            number);
+                                }
                             }
                         }
                     }
                 }
             }
-        }
+        });
     }
 
 }
