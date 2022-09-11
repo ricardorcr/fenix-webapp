f89afad176e37d6206691345c36a267f0a03fb9a
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-11 11:31:30 +0100

    Run CustomTask CheckCNAESCreatedAccounts

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/CheckCNAESCreatedAccounts.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/CheckCNAESCreatedAccounts.java
@@ -18,10 +18,12 @@ public class CheckCNAESCreatedAccounts extends ReadCustomTask {
                 .filter(account -> account.getIdentity().getAccountSet().size() > 1)
                 .forEach(account -> {
                     taskLog("%s : %s%n", account.getExternalId(), account.getEmail());
+                    taskLog("   %s%n", account.getCreatedInstant().toString("yyyy-mm-dd HH:mm"));
                     account.getIdentity().getAccountSet().stream()
                             .filter(a -> a != account)
-                            .map(a -> a.getEmail())
-                            .forEach(e -> taskLog("   %s%n", e));
+                            .forEach(a -> taskLog("   %s : %s%n",
+                                    a.getCreatedInstant().toString("yyyy-mm-dd HH:mm"),
+                                    a.getEmail()));
                 });
     }
 
