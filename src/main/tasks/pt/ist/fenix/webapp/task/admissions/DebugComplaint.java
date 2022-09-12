5216be7d0f3affa71b3491ec828758ef9b206e36
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-12 16:32:27 +0100

    Run CustomTask DebugComplaint

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/DebugComplaint.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/DebugComplaint.java
@@ -1,10 +1,17 @@
 package pt.ist.fenix.webapp.task.admissions;
 
 import org.fenixedu.admissions.domain.AdmissionProcess;
+import org.fenixedu.admissions.domain.ApplicationComplaint;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
 import org.fenixedu.connect.domain.Account;
+import org.fenixedu.connect.domain.ConnectSystem;
 import pt.ist.fenixframework.FenixFramework;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DebugComplaint extends ReadCustomTask {
 
@@ -12,15 +19,17 @@ public class DebugComplaint extends ReadCustomTask {
     public void runTask() throws Exception {
         final Account account = User.findByUsername("ist24439").getIdentity().getAccountSet().stream().findAny().orElse(null);
         final AdmissionProcess admissionProcess = FenixFramework.getDomainObject("852907490541641");
-        admissionProcess.getAdmissionProcessTargetSet().stream()
+        final Stream<ApplicationComplaint> complaints = admissionProcess.getAdmissionProcessTargetSet().stream()
                 .flatMap(target -> target.getApplicationSet().stream())
                 .filter(application -> application.isAllowedToView(account))
                 //.filter(application -> searchStrings == null || application.matches(searchStrings))
                 .flatMap(application -> application.getComplaintSet().stream())
                 //.filter(complaint -> open == null || complaint.isOpen() == open)
-                .forEach(applicationComplaint -> {
-                    taskLog("%s%n", applicationComplaint.getExternalId());
-                });
+        ;
+        final Map<Account, List<ApplicationComplaint>> groupedComplaints = complaints
+                .collect(Collectors.groupingBy(complaint -> ConnectSystem.getMostRelevantAccount(complaint.getApplication().getAccount())));
+        taskLog("%s%n", groupedComplaints.size());
+
     }
 
 }
