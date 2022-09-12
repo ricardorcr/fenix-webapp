61dc0cb817bc7636e523ff9537c54ac3b527686a
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-12 09:23:46 +0100

    Run CustomTask CheckProgress

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/CheckProgress.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/CheckProgress.java
@@ -1,5 +1,6 @@
 package pt.ist.fenix.webapp.task.admissions;
 
+import org.fenixedu.academic.domain.DomainOperationLog;
 import org.fenixedu.academic.domain.student.Registration;
 import org.fenixedu.admissions.domain.AdmissionProcess;
 import org.fenixedu.admissions.domain.AdmissionsSystem;
@@ -9,11 +10,12 @@ import org.fenixedu.admissions.ist.domain.RegistrationProcessState;
 import org.fenixedu.admissions.ist.domain.Survey;
 import org.fenixedu.admissions.ist.domain.UserAccountInfo;
 import org.fenixedu.admissions.ist.domain.Utils;
-import org.fenixedu.admissions.ist.service.AuthorizePersonalDataAccessService;
 import org.fenixedu.bennu.core.domain.User;
+import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
 import org.fenixedu.commons.spreadsheet.Spreadsheet;
 import org.fenixedu.connect.domain.Identity;
+import pt.ist.fenixedu.integration.domain.CardDataAuthorizationLog;
 
 import java.io.ByteArrayOutputStream;
 import java.util.stream.Stream;
@@ -80,7 +82,12 @@ public class CheckProgress extends ReadCustomTask {
                                 }
                             }
 
-                            if (AuthorizePersonalDataAccessService.hasCompletedAllDataAccessResponses(user)) {
+                            final long dataAuthRespo = user.getPerson().getDomainOperationLogsSet().stream()
+                                    .filter(CardDataAuthorizationLog.class::isInstance)
+                                    .sorted(DomainOperationLog.COMPARATOR_BY_WHEN_DATETIME.reversed())
+                                    .map(CardDataAuthorizationLog.class::cast)
+                                    .count();
+                            if (dataAuthRespo > 4l) {
                                 hasDataAuthorization++;
                             }
                         }
@@ -150,6 +157,8 @@ public class CheckProgress extends ReadCustomTask {
         row.setCell("Com Slot Confirmação Documentos", Long.toString(counter.hasSlotForDocumentConfirmation));
         row.setCell("Com Slot Visita Campus", Long.toString(counter.hasSlotForCampusVisit));
         row.setCell("Com Horário", Long.toString(counter.hasSchedule));
+
+
     }
 
     private boolean include(final AdmissionProcess admissionProcess) {
