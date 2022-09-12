838e3e440643746f2e32fcdb47d3371a4ef8f216
Ricardo da Conceição Rodrigues <ricardo.rodrigues@tecnico.ulisboa.pt>
2022-09-12 15:41:47 +0100

    Run CustomTask CheckAdmissionsOutcomeState

--- src/main/tasks/pt/ist/fenix/webapp/CheckAdmissionsOutcomeState.java
+++ src/main/tasks/pt/ist/fenix/webapp/CheckAdmissionsOutcomeState.java
@@ -84,34 +84,43 @@ public class CheckAdmissionsOutcomeState extends ReadCustomTask {
     }
 
     private void checkNeededChangeOutcomeState(final Application application) {
-        FenixFramework.atomic(() -> {
-            try {
-                final Enum outcomeState = Utils.outcomeStateFor(application);
-                if (outcomeState == RegistrationProcessState.BOARDING) {
-                    if (Utils.allMandatoryActivitiesDone(application)) {
-                        final AdmissionProcess admissionProcess = application.getAdmissionProcessTarget().getAdmissionProcess();
-                        if (Utils.isToChangeOutcomeState(admissionProcess)) {
-                            if (Utils.needsDocumentConfirmation(admissionProcess)) {
-                                taskLog("Should change for REGISTERED - %s - %s%n", application.getExternalId(), application.getAdmissionProcessTarget().getAdmissionProcess().getTitle().getContent());
-                                RegistrationService.setOutcomeState(application, RegistrationProcessState.REGISTERED);
-                                RegistrationService.addToConfirmationQueueIfNeeded(application);
-                            } else {
-                                taskLog("Should change for CONFIRMED - %s - %s%n", application.getExternalId(), application.getAdmissionProcessTarget().getAdmissionProcess().getTitle().getContent());
-                                RegistrationService.setOutcomeState(application, RegistrationProcessState.CONFIRMED);
-                                Signal.emit(RegistrationService.REGISTRATION_CONFIRMED, new DomainObjectEvent<>(application));
+        try {
+            FenixFramework.atomic(() -> {
+                try {
+                    final Enum outcomeState = Utils.outcomeStateFor(application);
+                    if (outcomeState == RegistrationProcessState.BOARDING) {
+                        if (Utils.allMandatoryActivitiesDone(application)) {
+                            final AdmissionProcess admissionProcess = application.getAdmissionProcessTarget().getAdmissionProcess();
+                            if (Utils.isToChangeOutcomeState(admissionProcess)) {
+                                if (Utils.needsDocumentConfirmation(admissionProcess)) {
+                                    taskLog("Should change for REGISTERED - %s - %s%n", application.getExternalId(), application.getAdmissionProcessTarget().getAdmissionProcess().getTitle().getContent());
+                                    RegistrationService.setOutcomeState(application, RegistrationProcessState.REGISTERED);
+                                    RegistrationService.addToConfirmationQueueIfNeeded(application);
+                                } else {
+                                    taskLog("Should change for CONFIRMED - %s - %s%n", application.getExternalId(), application.getAdmissionProcessTarget().getAdmissionProcess().getTitle().getContent());
+                                    RegistrationService.setOutcomeState(application, RegistrationProcessState.CONFIRMED);
+                                    Signal.emit(RegistrationService.REGISTRATION_CONFIRMED, new DomainObjectEvent<>(application));
+                                }
                             }
                         }
                     }
-                }
-            } catch (Throwable re) {
-                if (re.getCause() instanceof DomainException) {
+                } catch (Throwable re) {
+                    if (re.getCause() instanceof DomainException) {
+                        taskLog("#" + re.getCause().getMessage());
+                    }
+                    taskLog("$$" + re.getCause().getClass().getName());
                     taskLog("#" + re.getCause().getMessage());
+                    throw re;
                 }
-                taskLog("$$" + re.getCause().getClass().getName());
+            });
+        } catch (Throwable re) {
+            if (re.getCause() instanceof DomainException) {
                 taskLog("#" + re.getCause().getMessage());
-                throw re;
             }
-        });
+            taskLog("$$" + re.getCause().getClass().getName());
+            taskLog("#" + re.getCause().getMessage());
+            throw re;
+        }
     }
 
 }
