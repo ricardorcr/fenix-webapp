e3cf8458fa5c7aeb50bdc28babb93f4038140a7b
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-12 17:07:06 +0100

    Run CustomTask CheckTuitionConfig

---
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/CheckTuitionConfig.java
@@ -0,0 +1,44 @@
+package pt.ist.fenix.webapp.task.admissions;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.fenixedu.academic.domain.Degree;
+import org.fenixedu.academic.domain.accounting.EventTemplate;
+import org.fenixedu.admissions.domain.AdmissionProcess;
+import org.fenixedu.admissions.domain.AdmissionsSystem;
+import org.fenixedu.admissions.ist.domain.Utils;
+import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+import org.fenixedu.commons.spreadsheet.Spreadsheet;
+import pt.ist.fenixframework.FenixFramework;
+
+import java.io.ByteArrayOutputStream;
+
+public class CheckTuitionConfig extends ReadCustomTask {
+    @Override
+    public void runTask() throws Exception {
+        final Spreadsheet spreadsheet = new Spreadsheet("TuitionMap");
+
+        AdmissionsSystem.getInstance().getAdmissionProcessSet().stream()
+                .filter(admissionProcess -> admissionProcess.getTitle().getContent().indexOf("2023") > 0)
+                .flatMap(admissionProcess -> admissionProcess.getAdmissionProcessTargetSet().stream())
+                .forEach(target -> {
+                    final JsonObject confi = target.getOutcomeConfigJson();
+                    final JsonElement eventTemplate =  confi.get("eventTemplate");
+                    if (eventTemplate != null && !eventTemplate.isJsonNull()) {
+                        final Degree degree = FenixFramework.getDomainObject(confi.get("degree").getAsString());
+                        final EventTemplate eventTemplate1 = FenixFramework.getDomainObject(eventTemplate.getAsString());
+
+                        final Spreadsheet.Row row = spreadsheet.addRow();
+                        row.setCell("Concurso", target.getAdmissionProcess().getTitle().getContent());
+                        row.setCell("Degree", degree.getSigla());
+                        row.setCell("Plano", eventTemplate1.getCode());
+
+                    }
+                });
+
+
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        spreadsheet.exportToXLSSheet(stream);
+        output("tuitionMap.xlsx", stream.toByteArray());
+    }
+}
