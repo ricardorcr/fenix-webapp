f365e5c8ce6197eb76543b6b7426da48c0145aae
Luís Eugénio Marques Moreira Pires da Cruz <luis.cruz@tecnico.ulisboa.pt>
2022-09-12 13:15:43 +0100

    Run CustomTask AllocateCampusVisitSlot

--- src/main/tasks/pt/ist/fenix/webapp/task/admissions/AllocateCampusVisitSlot.java
+++ src/main/tasks/pt/ist/fenix/webapp/task/admissions/AllocateCampusVisitSlot.java
@@ -1,33 +1,37 @@
 package pt.ist.fenix.webapp.task.admissions;
 
+import org.fenixedu.admissions.domain.AdmissionProcessTarget;
 import org.fenixedu.admissions.domain.AdmissionsQueue;
 import org.fenixedu.admissions.domain.AdmissionsSystem;
+import org.fenixedu.admissions.domain.Application;
 import org.fenixedu.admissions.ist.domain.UserAccountInfo;
 import org.fenixedu.admissions.ist.domain.Utils;
 import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
-import org.fenixedu.bennu.scheduler.custom.WriteCustomTask;
+import pt.ist.fenixframework.FenixFramework;
 
-public class AllocateCampusVisitSlot extends WriteCustomTask {
+public class AllocateCampusVisitSlot extends ReadCustomTask {
 
     @Override
     public void runTask() throws Exception {
         AdmissionsSystem.getInstance().getAdmissionProcessSet().stream()
                 .flatMap(admissionProcess -> admissionProcess.getAdmissionProcessTargetSet().stream())
-                .forEach(target -> {
-                    final AdmissionsQueue admissionsQueue = target.getAfterOutcomeQueue();
-                    if (admissionsQueue != null) {
-                        target.getApplicationSet().stream()
-                                .filter(application -> Utils.registrationFor(application) != null)
-                                .filter(application -> UserAccountInfo.getSlotsNotFinished(application.getAccount()).stream()
-                                        .noneMatch(slot -> slot.getAttendanceQueue() == admissionsQueue.queue))
-                                .forEach(application -> {
-                                    taskLog("%s : %s%n",
-                                            target.getAdmissionProcess().getTitle().getContent(),
-                                            application.getAccount().getEmail());
-                                    admissionsQueue.allocateSlotFor(application);
-                                });
-                    }
-                });
+                .flatMap(target -> target.getApplicationSet().stream())
+                .forEach(this::process);
+    }
+
+    private void process(final Application application) {
+        FenixFramework.atomic(() -> {
+            final AdmissionProcessTarget target = application.getAdmissionProcessTarget();
+            final AdmissionsQueue admissionsQueue = target.getAfterOutcomeQueue();
+            if (admissionsQueue != null && Utils.registrationFor(application) != null &&
+                    UserAccountInfo.getSlotsNotFinished(application.getAccount()).stream()
+                            .noneMatch(slot -> slot.getAttendanceQueue() == admissionsQueue.queue)) {
+                taskLog("%s : %s%n",
+                        target.getAdmissionProcess().getTitle().getContent(),
+                        application.getAccount().getEmail());
+                admissionsQueue.allocateSlotFor(application);
+            }
+        });
     }
 
 }
